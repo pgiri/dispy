3f8d506df2aeef4fa9293dedeccb7230b979c081
pgiri <pgiri@yahoo.com>
2015-10-22 21:32:01 -0400

    Added support for job scheduler in client.
    Added 'submit_node' and 'send_file' methods to cluster. These,
    along with 'cluster_status' callback can be used to implement
    schedulers in client easily. See 'job_scheduler.py' for an example.

---
+++ py2/dispy/examples/provisional_results.py
@@ -0,0 +1,48 @@
+# Example to demonstrate a computation sending multiple "provisional"
+# results to the client. This feature can be used in optimizations
+# where each computation knows local optima and the client can collect
+# these as they are received to compute global optimal value
+
+import random, dispy
+
+def compute(n): # executed on nodes
+    import random, time, socket
+    name = socket.gethostname()
+    cur_best = 1
+    for i in xrange(0, n):
+        r = random.uniform(0, 1)
+        if r <= cur_best:
+            # possible result (local optimal value)
+            dispy_provisional_result((name, r))
+            cur_best = r
+        time.sleep(0.1)
+    # final result
+    return (name, cur_best)
+
+# dispy calls this function to indicate change in job status
+def job_callback(job): # executed at the client
+    if job.status == dispy.DispyJob.ProvisionalResult or \
+       job.status == dispy.DispyJob.Finished:
+        print('best value from %s: %s' % (job.result[0], job.result[1]))
+        if job.result[1] < 0.005:
+            # acceptable result; terminate jobs
+            print('%s computed: %s' % (job.result[0], job.result[1]))
+            # 'jobs' and 'cluster' are created in '__main__' below
+            for j in jobs:
+                if j.status in [dispy.DispyJob.Created, dispy.DispyJob.Running,
+                                dispy.DispyJob.ProvisionalResult]:
+                    cluster.cancel(j)
+
+if __name__ == '__main__':
+    cluster = dispy.JobCluster(compute, callback=job_callback)
+    jobs = []
+    for n in range(4):
+        job = cluster.submit(random.randint(50,100))
+        if job is None:
+            print('creating job %s failed!' % n)
+            continue
+        job.id = n
+        jobs.append(job)
+    cluster.wait()
+    cluster.stats()
+    cluster.close()
---
+++ py3/dispy/examples/job_scheduler.py
@@ -0,0 +1,58 @@
+# This program registers 'cluster_callback' which schedules
+# computations when a processor is available.
+
+# job computation runs at dispynode servers
+def compute(path):
+    import hashlib, time, os
+    csum = hashlib.sha1()
+    with open(os.path.basename(path), 'rb') as fd:
+        while True:
+            data = fd.read(1024000)
+            if not data:
+                break
+            csum.update(data)
+    time.sleep(5)
+    return csum.hexdigest()
+
+
+# 'cluster_status' callback function. It is called by dispy (client)
+# to indicate node / job status changes. Here node iniitialization and
+# job done status are used to schedule jobs, so at most one job is
+# running on a node (even if a node has more than one processor). Data
+# files are assumed to be 'data000', 'data001' etc.
+def status_cb(status, node, job):
+    if status == dispy.DispyJob.Finished:
+        print('sha1sum for %s: %s' % (job.id, job.result))
+    elif status == dispy.DispyJob.Terminated:
+        print('sha1sum for %s failed: %s' % (job.id, job.exception))
+    elif status == dispy.DispyNode.Initialized:
+        print('node %s with %s CPUs available' % (node.ip_addr, node.avail_cpus))
+    else:
+        return
+
+    global submitted
+    data_file = 'data%03d' % submitted
+    if os.path.isfile(data_file):
+        submitted += 1
+        # 'node' and 'dispy_job_depends' are consumed by dispy;
+        # 'compute' is called with only 'data_file' as argument(s)
+        job = cluster.submit_node(node, data_file, dispy_job_depends=[data_file])
+        job.id = data_file
+            
+
+if __name__ == '__main__':
+    import dispy, sys, os, threading, logging
+    nodes = ['172.16.10.25']
+    # nodes = ['*']
+    cluster = dispy.JobCluster(compute, nodes=nodes, cluster_status=status_cb, loglevel=logging.DEBUG)
+    submitted = 0
+    while True:
+        try:
+            cmd = sys.stdin.readline().strip().lower()
+        except KeyboardInterrupt:
+            break
+        if cmd == 'quit' or cmd == 'exit':
+            break
+
+    cluster.wait()
+    cluster.stats()
