c6b76427886137cddb081edb4965357ab0a28cad
Giridhar Pemmasani <pgiri@yahoo.com>
2016-07-10 02:31:43 -0400

    Updated version to 4.6.15.
    Updated version number to 4.6.15.

--- py3/dispy/__init__.py
+++ py3/dispy/__init__.py
@@ -37,7 +37,7 @@ __maintainer__ = "Giridhar Pemmasani (pgiri@yahoo.com)"
 __license__ = "MIT"
 __url__ = "http://dispy.sourceforge.net"
 __status__ = "Production"
-__version__ = "4.6.14"
+__version__ = "4.6.15"
 
 __all__ = ['logger', 'DispyJob', 'DispyNode', 'NodeAllocate', 'JobCluster', 'SharedJobCluster']
 
