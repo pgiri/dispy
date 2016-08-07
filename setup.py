85fa1b74b7227379b0a9fdc9b35901b8c52bf277
Giridhar Pemmasani <pgiri@yahoo.com>
2016-08-07 14:12:59 -0400

    Marked version as 4.6.15

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='dispy',
-    version='4.6.14',
+    version='4.6.15',
     description='Distributed and Parallel Computing with/for Python.',
     long_description=open('README.rst').read(),
     keywords='distributed computing, parallel processing, mapreduce, hadoop, job scheduler',
