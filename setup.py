10746244f671701fcdd8f9931852e2665759c21e
Giridhar Pemmasani <pgiri@yahoo.com>
2017-01-07 11:40:07 -0500

    Updated dependency on asyncoro.
    asyncoro version 4.4.0 fixes SSL issues, so updated dependency to
    that version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     package_data = {
         'dispy' : ['data/*', 'examples/*', 'doc/*'],
     },
-    install_requires=['asyncoro >= 4.3.4'],
+    install_requires=['asyncoro >= 4.4.0'],
     scripts=[os.path.join(base_dir, 'dispy', script) for script in \
              ['dispynode.py', 'dispynetrelay.py', 'dispyscheduler.py']] + \
             [os.path.join(base_dir, script) for script in ['dispy.py']],
