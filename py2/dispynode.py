bbc4125a23129371fde18d38807170f9f87c27ea
Giridhar Pemmasani <pgiri@yahoo.com>
2021-01-26 20:40:28 -0500

    Set the interpreter path for version used.
    Set interpreter path to python3 or python2 for versions appropriately.

--- py2/dispynode.py
+++ py2/dispynode.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 """
 dispynode: Execute computations on behalf of dispy clients;
--- py3/dispynode.py
+++ py3/dispynode.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """
 dispynode: Execute computations on behalf of dispy clients;
